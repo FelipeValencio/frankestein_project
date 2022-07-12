3cbc6b251270a13c6e1ba33464f4d958734e52d1
Felipe Valencio <fgv.valencio@gmail.com>
2022-07-11 22:36:22 -0300

    add cowsay

--- server.js
+++ server.js
@@ -1,12 +1,31 @@
+var cowsay = require("cowsay");
+
 const express = require('express')
 const app = express()
 
-const port = process.env.PORT || 80
+const port = process.env.PORT || 8080
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
+app.get("/cow", function (req, res) {
+    let text;
+  
+    text = req.query.text;
+    
+    if(text == undefined)
+        text = "Hi please dont hack me!";
+    
+    const responseText = `
+      <pre>${cowsay.say({ text })}</pre>
+      <br/><br/>
+    `;
+  
+    res.send(responseText);
+  }
+);
+
+app.listen(port, '127.0.0.1', () => {
   console.log(`Example app listening on port ${port}`)
 })
